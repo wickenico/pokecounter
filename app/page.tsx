fc4dd985f3901786b892ad6e24b3b6d4dea76525
Nico Wickersheim <nico.wickersheim@sevdesk.de>
2025-03-11 20:56:06 +0100

    Improve UI

--- app/page.tsx
+++ app/page.tsx
@@ -189,7 +189,7 @@ export default function Home() {
                         type="text"
                         value={pokemonName}
                         onChange={(e) => setPokemonName(e.target.value)}
-                        placeholder="Pokémon Name"
+                        placeholder="Pokémon eingeben..."
                         className="border border-gray-300 rounded-lg shadow-sm px-3 py-2 focus:ring-2 focus:ring-blue-500"
                     />
                     <Button
